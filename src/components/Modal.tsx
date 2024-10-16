1149f82edde24fe0c330e93d91db8cb025abd2a5
Felipe <dfsilvat@gmail.com>
2024-10-16 11:58:49 -0300

    adjust: text modals

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ const Modal: React.FC<ModalProps>  = ({ isOpen, children }) => {
     return (
         <div className="h-screen w-screen backdrop-blur-sm	 fixed top-0 left-0 flex items-center justify-center p-4">
             <div className="w-full p-1 bg-white flex flex-col items-center justify-center p-2 rounded">
-                <h1 className='text-xl font-bold text-center mt-4'>Preencha as seguintes indormações</h1>
+                <h1 className='text-xl font-bold text-center mt-4'>Preencha as seguintes informações</h1>
                 {children}
             </div>
         </div>
