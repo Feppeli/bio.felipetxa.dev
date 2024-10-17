abfd4722e8b7b4f0c9ed7ba1cf36e50504ca3d65
Felipe <dfsilvat@gmail.com>
2024-10-17 02:07:02 -0300

    add: scroll reveal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Modal from './components/Modal'
+import ModalCourse from './components/ModalCourse'
 
-import { useState } from 'react'
+
+import { useState, useRef, useEffect } from 'react'
+import ScrollReveal from 'scrollreveal'
 
 import img1 from './assets/curso de programacao.png'
 import img2 from './assets/landingpage de advogado.png'
 import img3 from './assets/orçamento.png'
 import img4 from './assets/sistema.png'
-import ModalCourse from './components/ModalCourse'
 
 function App() {
 
@@ -35,9 +37,29 @@ function App() {
     window.location.href = "https://wa.me/5581991274483?text=Olá.+preciso+de+uma+landing+page+para+o+meu+escritório!"
   }
 
-  // const getOrcamento = () => {
-  //   window.location.href = "  "
-  // }
+  // scroll reveal configuration
+  const elementsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+    useEffect(() => {
+        const sr = ScrollReveal({
+            distance: '50px',
+            duration: 1000,
+            easing: 'ease-in-out',
+            opacity: 0,
+            scale: 0.85,
+        });
+
+        elementsRef.current.forEach((el, index) => {
+            if (el) {
+                sr.reveal(el, {
+                    interval: 200,
+                    delay: index * 100, // Delay incremental para cada elemento
+                });
+            }
+        });
+    }, []);
+
+  
 
   return (
     <>
@@ -50,22 +72,22 @@ function App() {
             <div className='flex flex-col md:grid grid-cols-2 gap-4 w-3/4 h-3/4'>
 
               {/* Elementos do grid */}
-              <div className='row-start-1 row-end-3 p-5 md:p-10 rounded-xl bg-cover bg-center hover:grayscale cursor-pointer delay-500 md:hover:p-6 md:hover:m-6 easy-in-out duration-500' style={{ backgroundImage: `url(${img2})` }} onClick={redirectToWhatsapp}>
+              <div ref={el => (elementsRef.current[0] = el)} className='row-start-1 row-end-3 p-5 md:p-10 rounded-xl bg-cover bg-center hover:grayscale cursor-pointer delay-500 md:hover:p-6 md:hover:m-6 easy-in-out duration-500' style={{ backgroundImage: `url(${img2})` }} onClick={redirectToWhatsapp}>
                 <div className='flex items-left justify-center flex-col w-full h-full'>
                   <h1 className='text-4xl font-normal text-white'>Landingpages<br /> <span className='text-2xl font-thin'>para advogados e escritórios de advocacia</span></h1>
                   <h3 className=' text-2xl md:text-4xl text-white pt-4 font-bold'>A partir de:<span className='text-4xl font-semibold text-green-600'>R$699,90</span></h3>
                 </div>
               </div>
 
-              <div className='col-start-2 p-5 md:p-10  rounded-xl bg-cover bg-center flex items-left justify-center flex-col w-full h-full hover:grayscale ease-in-out cursor-pointer delay-500 md:hover:p-6 md:hover:m-6 easy-in-out duration-500' style={{ backgroundImage: `url(${img1})` }} onClick={openModal}>
+              <div ref={el => (elementsRef.current[1] = el)} className='col-start-2 p-5 md:p-10  rounded-xl bg-cover bg-center flex items-left justify-center flex-col w-full h-full hover:grayscale ease-in-out cursor-pointer delay-500 md:hover:p-6 md:hover:m-6 easy-in-out duration-500' style={{ backgroundImage: `url(${img1})` }} onClick={openModal}>
                 <h1 className=' text-center text-3xl md:text-4xl font-bold text-white '>Realizar Orçamento</h1>
               </div>
 
-              <div className='col-start-2 p-5 md:p-10  rounded-xl bg-cover bg-center flex items-left justify-center flex-col w-full h-full hover:grayscale ease-in-out cursor-pointer delay-500 md:hover:p-6 md:hover:m-6 easy-in-out duration-500' style={{ backgroundImage: `url(${img3})` }} onClick={openModal}>
+              <div ref={el => (elementsRef.current[2] = el)}className='col-start-2 p-5 md:p-10  rounded-xl bg-cover bg-center flex items-left justify-center flex-col w-full h-full hover:grayscale ease-in-out cursor-pointer delay-500 md:hover:p-6 md:hover:m-6 easy-in-out duration-500' style={{ backgroundImage: `url(${img3})` }} onClick={openModal}>
                 <h1 className='text-center text-3xl md:text-4xl font-bold text-white'>Preciso de um Site!</h1>
               </div>
 
-              <div className='col-start-1 col-end-3 p-5 md:p-10 rounded-xl bg-cover bg-center flex items-left justify-center flex-col w-full h-full hover:grayscale ease-in-out cursor-pointer delay-500 md:hover:p-6 md:hover:m-6 easy-in-out duration-500' style={{ backgroundImage: `url(${img4})` }} onClick={openCourseModal}>
+              <div ref={el => (elementsRef.current[3] = el)}className='col-start-1 col-end-3 p-5 md:p-10 rounded-xl bg-cover bg-center flex items-left justify-center flex-col w-full h-full hover:grayscale ease-in-out cursor-pointer delay-500 md:hover:p-6 md:hover:m-6 easy-in-out duration-500' style={{ backgroundImage: `url(${img4})` }} onClick={openCourseModal}>
                 <h1 className='text-left text-2xl md:text-4xl font-normal text-white'>Curso de programação<br /> web Full Stack <span className=' text-2xl md:text-4xl font-black text-white'>(em breve)</span></h1>
               </div>
 
